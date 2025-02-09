62200752c1d332be6fe08bcc0bc1a51ec9286dd1
MeshariAlsh <meshari.alsh21@gmail.com>
2025-02-09 23:17:40 +0000

    Added a func to generate an extra spirl

--- spiral_lines.py
+++ spiral_lines.py
@@ -15,7 +15,7 @@ Head = (centerWindowPos[0], centerWindowPos[1] - 100)
 Tail = (centerWindowPos[0], centerWindowPos[1])
 LINE_WIDTH = 5
 NUM_OF_LINES = 5 
-NUM_OF_L = 12 
+NUM_OF_L = 20
 
 myLines = []
 storePointsfrom = []
@@ -25,20 +25,17 @@ diffcolor = (255, 255, 0)
 lineColour = (255, 0, 0)
 HeadTwo = (centerWindowPos[0], centerWindowPos[1] - 100)
 TailTwo = (centerWindowPos[0], centerWindowPos[1])
-LINE_WIDTH = 5
-NUM_OF_LINES = 5 
-myLines = []
-storePointsfrom = []
+
 
 # Second Line information 
-diffcolor = (255, 255, 0)
-lineColour = (255, 0, 0)
 HeadThree = (centerWindowPos[0], centerWindowPos[1] - 100)
 TailThree = (centerWindowPos[0], centerWindowPos[1])
-LINE_WIDTH = 5
-NUM_OF_LINES = 5 
-myLines = []
-storePointsfrom = []
+
+
+# Second Line information 
+HeadFour = (centerWindowPos[0], centerWindowPos[1] - 100)
+TailFour = (centerWindowPos[0], centerWindowPos[1])
+
 
 # Function uses derived formula from  L_1 metric unit sphere to find points in the 4th quadrant
 def ComputeNewLineFourthQuadrant(distance, prevHead):
@@ -79,7 +76,7 @@ def CalculateHypotenuse(Oppistite, counter, positionOfSpiral):
     return Hypotenuse
 
 
-def GenerateNewLinesTOP( prevLineHead, prevLineTail, counter):
+def GenerateNewLinesRightDown( prevLineHead, prevLineTail, counter):
 
     # Identification string
     positionOfSpiral = "TOP Hand Side"
@@ -97,6 +94,29 @@ def GenerateNewLinesTOP( prevLineHead, prevLineTail, counter):
          
     else:
          newLineHead = ComputeNewLineFourthQuadrant(radius, prevLineHead)
+   
+    print(f'TOP Hand Side: New point after function compute newline: {newLineHead} Loop No {counter}. \n')
+
+    return newLineHead
+
+def GenerateNewLinesLeftDown( prevLineHead, prevLineTail, counter):
+
+    # Identification string
+    positionOfSpiral = "TOP Hand Side"
+                
+    # To get the radius of the L_1 unit sphere respective to previous head 
+    lenOfPhantomLine = (prevLineTail[1] - prevLineHead[1]) 
+    Hypotenuse = CalculateHypotenuse(lenOfPhantomLine, counter, positionOfSpiral) 
+    radius = int(Hypotenuse)
+
+    print(f'TOP Hand Side: Length of the radius in Manhattan Metric unit sphere {radius} Loop No {counter}. \n')
+
+    # Counter is used as a switch to change the direction of the each line generated  
+    if ( counter % 2 == 0):
+        newLineHead = ComputeNewLineThirdQuadrant(radius, prevLineHead)
+         
+    else:
+         newLineHead = ComputeNewLineFourthQuadrant(-radius, prevLineHead)
    
     print(f'TOP Hand Side: New point after function compute newline: {newLineHead} Loop No {counter}. \n')
 
@@ -158,6 +178,9 @@ def main():
     global TailThree # Second Spiral
     global HeadThree
 
+    global TailFour # Second Spiral
+    global HeadFour
+
 
     # Generate Lines
     for lines in range(NUM_OF_LINES):
@@ -173,10 +196,17 @@ def main():
         HeadTwo = newLineHeadTwo
 
     for lines in range(NUM_OF_LINES):
-        newLineHeadThree = GenerateNewLinesTOP(HeadThree , TailThree, lines)
+        newLineHeadThree = GenerateNewLinesRightDown(HeadThree , TailThree, lines)
         myLines.append([newLineHeadThree, HeadThree])
         TailThree = HeadThree
         HeadThree = newLineHeadThree
+
+    for lines in range(NUM_OF_LINES):
+        newLineHeadFour = GenerateNewLinesLeftDown(HeadFour , TailFour, lines+1)
+        myLines.append([newLineHeadFour, HeadFour])
+        TailFour = HeadFour
+        HeadFour = newLineHeadFour
+
 
     # Game loop
     running = True
@@ -192,7 +222,7 @@ def main():
                  pygame.draw.line(window, lineColour,myLines[lines][0], myLines[lines][1] , LINE_WIDTH)
         
         for lines in range(4):
-            pygame.draw.line(window, diffcolor,myLines[lines+11][0], myLines[lines+11][1] , LINE_WIDTH)
+            pygame.draw.line(window, lineColour,myLines[lines+11][0], myLines[lines+11][1] , LINE_WIDTH)
 
 
 
