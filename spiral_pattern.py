f83e859964346d57dd2df484f8a19f26502914da
MeshariAlsh <meshari.alsh21@gmail.com>
2025-02-16 23:01:16 +0000

    refactored the def main into two functions that handle pattern and row generation

--- spiral_pattern.py
+++ spiral_pattern.py
@@ -4,78 +4,78 @@ import time
 import Spiral
 
 pygame.init()
-
 window = pygame.display.set_mode((1000,1000))
 sizeWindow = pygame.display.get_window_size()
-centerWindowPos = (sizeWindow[0] / 2 , sizeWindow[1] /2) # To get the coords for Head and Tail of the origin line
+centerWindowPos = (sizeWindow[0] / 2 , sizeWindow[1] /2) 
 
 # Line information 
 diffcolor = (255, 255, 0)
 lineColour = (0, 0, 0)
 Head = (centerWindowPos[0], centerWindowPos[1] - 100)
 Tail = (centerWindowPos[0], centerWindowPos[1])
-LINE_WIDTH = 5
-NUM_OF_LINES = 5 
-NUM_OF_RENDERED_LINES = 2
+LINE_WIDTH = 7
+NUM_OF_SPIRAL_VARIANTS = 4
 myLines = []
 drawing = []
 
+# Row pattern information
+x_step =  200 
+y_offset = 50
+ 
+def generate_patterns(offset_multiplier, x_step , y_offset, row_type):
+     
+     if row_type == "top":
+        y_head_multiplier = - 5
+        y_tail_multiplier = - 3
+        Head = (centerWindowPos[0], centerWindowPos[1] - 300)
+        Tail = (centerWindowPos[0], centerWindowPos[1] - 200)
+        myLines.append([Head, Tail])
+
+     elif row_type == "middle":
+        y_head_multiplier = - 1
+        y_tail_multiplier = 1
+           
+     elif row_type == "bottom":
+        y_head_multiplier = 3
+        y_tail_multiplier = 5
+        Head = (centerWindowPos[0], centerWindowPos[1] + 100)
+        Tail = (centerWindowPos[0], centerWindowPos[1] + 200)
+        myLines.append([Head, Tail])
+           
+     for j in range(2,4):
+        sign = (-1) ** j 
+        Head = (centerWindowPos[0] + ( offset_multiplier * x_step * sign), centerWindowPos[1] + (y_offset * y_head_multiplier)- (y_offset * (offset_multiplier - 1)))
+        Tail = (centerWindowPos[0] + ( offset_multiplier * x_step * sign), centerWindowPos[1] + (y_offset * y_tail_multiplier)- (y_offset * (offset_multiplier - 1)))
+        print(f"Value of Head: {Head} and Tail: {Tail}  ___{row_type}__")
+        myLines.append([Head, Tail])
+      
+def generate_row (row_type):
+      
+    for offset_multiplier in range(1,3):
+         generate_patterns(offset_multiplier, x_step, y_offset, row_type)
+            
 def main():
 
     global Head
     global Tail
+    global myLines
 
     spiral1 = Spiral.Spiral(Head, Tail)
     myLines.append([Head, Tail])
+       
+    generate_row("middle")
+    generate_row("top")
+    generate_row("bottom")
 
-    for row_of_patterns in range(5):
-        for p in range(1,3):
-            if len(myLines) < 5:
-                for j in range(2,4):
-                    if len(myLines) < 5:
-
-                        sign = (-1) ** j 
-                        Head = (centerWindowPos[0] + ( p * 200 * sign), centerWindowPos[1] - 50 - (50 * (p - 1)))
-                        Tail = (centerWindowPos[0] + ( p * 200 * sign), centerWindowPos[1] + 50 - (50 * (p - 1)))
-                        myLines.append([Head, Tail])
-                        
-            # Top row 
-            elif len(myLines) >= 5 and len(myLines) <= 9 :
-                #initial the center of the row
-                if len(myLines) == 5  and len(myLines) < 6:
-                        Head = (centerWindowPos[0], centerWindowPos[1] - 300)
-                        Tail = (centerWindowPos[0], centerWindowPos[1] - 200)
-                        myLines.append([Head, Tail])
-                        
-                for j in range(2,4):
-
-                    sign = (-1) ** j 
-                    Head = (centerWindowPos[0] + ( p * 200 * sign), centerWindowPos[1] - 230 - (70 * (p - 1)))
-                    Tail = (centerWindowPos[0] + ( p * 200 * sign), centerWindowPos[1] - 130 - (70 * (p - 1)))
-                    myLines.append([Head, Tail])
-                   
-            # Bottom Row
-            elif len(myLines) >= 10 and len(myLines) <= 15 :
-                    #initial the center of the row
-                if len(myLines) == 10  and len(myLines) < 11:
-                        Head = (centerWindowPos[0], centerWindowPos[1] + 100)
-                        Tail = (centerWindowPos[0], centerWindowPos[1] + 200)
-                        myLines.append([Head, Tail])
-                        
-                for j in range(2,4):
-                    sign = (-1) ** j 
-                    Head = (centerWindowPos[0] + ( p * 200 * sign), centerWindowPos[1] + 150 - (50 * (p - 1)))
-                    Tail = (centerWindowPos[0] + ( p * 200 * sign), centerWindowPos[1] + 250 - (50 * (p - 1)))
-                    myLines.append([Head, Tail])
-                   
-    for j in range (15):
-        for i in range(4):
-            spiral1 = Spiral.Spiral(myLines[j][0], myLines[j][1])
-            spiral1.GenerateSpiralPattern(i)
+    print(f"Size of myline: {len(myLines)} \n")
+    
+    for line in range (len(myLines)):
+        for variant in range(NUM_OF_SPIRAL_VARIANTS):
+            spiral1 = Spiral.Spiral(myLines[line][0], myLines[line][1])
+            spiral1.GenerateSpiralPattern(variant)
             drawing.append(spiral1.lines)
-
-            spiral1.head= myLines[j][0]
-            spiral1.tail = myLines[j][1]
+            spiral1.head= myLines[line][0]
+            spiral1.tail = myLines[line][1]
 
     # Game loop
     running = True
@@ -85,7 +85,7 @@ def main():
                 running = False 
         
         window.fill((245, 245, 220))
-        
+
         # Render Lines
         for patterns in drawing:
                     for lines in patterns:
